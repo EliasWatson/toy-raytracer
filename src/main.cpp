ad0a621f3b7de8964b9fb5e1663a8e5db5f90541
Elias Watson <yt.assossa@gmail.com>
2020-10-15 13:55:18 -0400

    Camera & anti-aliasing

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,27 @@
 #include "util.h"
 #include "visible_list.h"
 #include "sphere.h"
+#include "camera.h"
 
 #include "float.h"
+#include <random>
 
 vec3 sky_color(const ray& r) {
 	float t = 0.5 * (r.dir.y + 1.0);
 	return mix(vec3(1.0), vec3(0.5, 0.7, 1.0), t);
 }
 
-void render(image* img) {
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0);
+vec3 get_pixel(ray& r, visible_list& world) {
+	collision hit;
+	if(world.intersects(r, 0.0, MAXFLOAT, hit)) {
+		return 0.5 * vec3(hit.normal.x + 1.0, hit.normal.y + 1.0, hit.normal.z + 1.0);
+	} else {
+		return sky_color(r);
+	}
+}
+
+void render(image* img, int samples) {
+	camera cam;
 
 	std::vector<visible*> obj_list;
 	obj_list.push_back(new sphere(vec3(0.0, 0.0, -1.0), 0.5));
@@ -26,19 +34,16 @@ void render(image* img) {
 
 	for(int y = 0; y < img->height; ++y) {
 		for(int x = 0; x < img->width; ++x) {
-			float u = float(x) / float(img->width);
-			float v = float(y) / float(img->height);
-
-			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-			collision hit;
 			vec3 col;
 
-			if(objects.intersects(r, 0.0, MAXFLOAT, hit)) {
-				col = 0.5 * vec3(hit.normal.x + 1.0, hit.normal.y + 1.0, hit.normal.z + 1.0);
-			} else {
-				col = sky_color(r);
+			for(int i = 0; i < samples; ++i) {
+				float u = float(x + drand48()) / float(img->width);
+				float v = float(y + drand48()) / float(img->height);
+				ray r = cam.get_ray(u, v);
+				col += get_pixel(r, objects);
 			}
 
+			col /= float(samples);
 			img->set_pixel(x, img->height - y - 1, col);
 		}
 	}
@@ -47,8 +52,9 @@ void render(image* img) {
 int main() {
 	const int width = 800;
 	const int height = 400;
+	const int samples = 32;
 
 	image img(width, height);
-	render(&img);
+	render(&img, samples);
 	img.save("test.ppm");
 }
