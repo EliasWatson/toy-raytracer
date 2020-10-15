3e7c38a621a8e5f4874e851f623e36f6174251da
Elias Watson <yt.assossa@gmail.com>
2020-10-15 13:06:02 -0400

    Sphere normals

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,20 @@
 #include "ray.h"
 #include "util.h"
 
-vec3 color(const ray& r) {
+vec3 sky_color(const ray& r) {
 	float t = 0.5 * (r.dir.y + 1.0);
 	return mix(vec3(1.0), vec3(0.5, 0.7, 1.0), t);
+}
+
+float hit_sphere(const vec3& center, float radius, const ray& r) {
+	vec3 oc = r.pos - center;
+	float a = dot(r.dir, r.dir);
+	float b = 2.0 * dot(oc, r.dir);
+	float c = dot(oc, oc) - radius*radius;
+	float discriminant = b*b - 4.0*a*c;
+
+	if(discriminant < 0) return -1.0;
+	else return (-b - sqrt(discriminant)) / (2.0 * a);
 }
 
 void render(image* img) {
@@ -20,7 +31,15 @@ void render(image* img) {
 			float v = float(y) / float(img->height);
 
 			ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-			vec3 col = color(r);
+			float dist = hit_sphere(vec3(0.0, 0.0, -1.0), 0.5, r);
+
+			vec3 col;
+			if(dist > 0.0) {
+				vec3 nml = normalize(r.extend(dist) - vec3(0.0, 0.0, -1.0));
+				col = 0.5 * vec3(nml.x + 1.0, nml.y + 1.0, nml.z + 1.0);
+			} else {
+				col = sky_color(r);
+			}
 
 			img->set_pixel(x, img->height - y - 1, col);
 		}
@@ -28,8 +47,8 @@ void render(image* img) {
 }
 
 int main() {
-	const int width = 640;
-	const int height = 480;
+	const int width = 800;
+	const int height = 400;
 
 	image img(width, height);
 	render(&img);
